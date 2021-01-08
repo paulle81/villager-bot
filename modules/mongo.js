3fe7f8cc8d07e15013232223bef30572dfffdb4c
Paul Le <paul.le@bjss.com>
2021-01-08 17:34:05 +0000

    Remote mongo

--- modules/mongo.js
+++ modules/mongo.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
-const { mongoPath } = require('../config.json');
 
 module.exports = async () => {
-    await mongoose.connect(mongoPath, {
+    await mongoose.connect(process.env.MONGO_PATH, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
